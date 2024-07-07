bca0170ebc2c5b151c43c3b71a4c78f9640022b8
NancyBabe <Samuelnancy37@gmail.com>
2024-07-07 23:45:34 +0100

    initial commit of my simple counter app

---
+++ counter.js
@@ -0,0 +1,106 @@
+const count = document.getElementById('count')
+let counter = 0;
+
+function increaseBtn() {
+counter = counter + 1;
+count.innerHTML =counter
+}
+
+function decreaseBtn() {
+ counter = counter - 1;
+ count.innerHTML = counter
+ }
+
+ function doublex2() {
+ counter=counter *2;
+ count.innerHTML = counter
+ }
+
+function reset() {
+counter=0;
+  count.innerHTML = counter
+}
+
+function square() {
+ counter=counter * counter;
+count.innerHTML = counter
+}
+
+function x5() {
+  counter=counter *5;
+  count.innerHTML = counter
+  } 
+
+function cube() {
+  counter=counter *counter * counter;
+  count.innerHTML=counter
+ }
+
+ function x10 () {
+  counter=counter *10;
+  count.innerHTML =counter
+  } 
+
+function x15() {
+  counter=counter *15;
+  count.innerHTML = counter
+}
+
+function squareroot() {
+  counter=Math.sqrt(counter);
+  count.innerHTML = counter
+  } 
+
+function x25() {
+  counter=counter *25;
+  count.innerHTML=counter
+}
+
+function x3() {
+  counter=counter *3;
+  count.innerHTML =counter
+  } 
+
+function x7() {
+  counter=counter *7;
+  count.innerHTML=counter
+}
+
+function x9() {
+  counter=counter *9;
+  count.innerHTML =counter
+  } 
+
+function x12() {
+  counter=counter *12;
+  count.innerHTML=counter
+}
+
+function x50() {
+  counter=counter *50;
+  count.innerHTML =counter
+  } 
+
+function add4() {
+  counter=counter +4;
+  count.innerHTML=counter
+}
+
+function divideby5() {
+  counter=counter /5;
+  count.innerHTML =counter
+  } 
+
+function divideby15() {
+  counter=counter /15;
+  count.innerHTML=counter
+}
+
+function divideby35() {
+counter=counter /35;
+count.innerHTML=counter
+}
+
+
+
+
